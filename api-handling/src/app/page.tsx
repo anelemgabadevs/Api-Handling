1e5fc7e21d9b198c2c6f7798799f6d26809584bc
Anele Mgaba <anelemgabaa@gmail.com>
2023-11-09 11:36:20 -0800

    Api handling implementation

--- api-handling/src/app/page.tsx
+++ api-handling/src/app/page.tsx
@@ -1,113 +1,165 @@
-import Image from 'next/image'
+'use client'
+import MainCard from "@/components/MainCard";
+import React, { useState, useEffect } from 'react';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+export default function MainPage() {
+  const [apiResponseData, setApiResponseData] = useState<any>(null);
+  const [errorDisplay,setErrorDisplay] = useState<any>(false)
+  const [showErrorDiv, setShowErrorDiv] = useState<any>(false)
+
+  // Creating an array of objects to hold state for each card
+  const [cardStates, setCardStates] = useState<any[]>([
+    { responseStatus: '', responseDescription: '', textColor:'', ctaText:'FireApiCall', isLoading: false },
+    { responseStatus: '', responseDescription: '',  textColor:'', ctaText:'FireApiCall', isLoading: false },
+    { responseStatus: '', responseDescription: '' ,  textColor:'', ctaText:'FireApiCall', isLoading: false },
+    { responseStatus: '', responseDescription: '' ,  textColor:'', ctaText:'FireApiCall', isLoading: false },
+    { responseStatus: '', responseDescription: '',  textColor:'', ctaText:'FireApiCall', isLoading: false },
+    { responseStatus: '', responseDescription: '' ,  textColor:'', ctaText:'FireApiCall', isLoading: false },
+  ]);
+
+  const LoadingSpinner = () =>{
+    return (
+      <div
+      className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status">
+      <span
+        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+        >Loading...</span>
+    </div>
+    )
+  }
+
+  // Reusable async function for specific API calls
+  async function HandleGlobalApiRequest(url: string, index: number) {
+    try {
+      // Setting loading to true for the specific card at the start of the handleclick function
+      setCardStates((prevStates) => [
+        ...prevStates.slice(0, index),
+        { ...prevStates[index], isLoading: true },
+        ...prevStates.slice(index + 1),
+      ]);
+
+      const apiResponse = await fetch(url, {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      const loadingText = cardStates[index].isLoading ? '.....' : 'FireApiCall';
+
+      if (!apiResponse.ok) {
+        const responseData = await apiResponse.json();
+
+        // Updating card states
+        setCardStates((prevStates) => [
+          ...prevStates.slice(0, index),
+          {
+            responseStatus: apiResponse.status,
+            responseDescription: responseData.description,
+            textColor: 'text-red-600 font-600',
+            ctaText: loadingText,
+            isLoading: false, // Seting loading to false after the API call is completed
+          },
+          ...prevStates.slice(index + 1),
+        ]);
+      } else {
+        const responseData = await apiResponse.json();
+
+        // Updating card states
+        setCardStates((prevStates) => [
+          ...prevStates.slice(0, index),
+          {
+            responseStatus: apiResponse.status,
+            responseDescription: responseData.description,
+            textColor: 'text-green-600 font-600',
+            ctaText: loadingText,
+            isLoading: false, // Setting loading to false after the API call is completed
+          },
+          ...prevStates.slice(index + 1),
+        ]);
+
+        const data = await apiResponse.json();
+        setApiResponseData(data);
+      }
+    } catch (error) {
+      setErrorDisplay(true)
+      console.log('The following error occurred: ', error);
+      setCardStates((prevStates) => [
+        ...prevStates.slice(0, index),
+        {
+          ctaText: 'FireApiCall',
+          isLoading: false, // Settting loading to false when there is an error
+        },
+        ...prevStates.slice(index + 1),
+      ]);
+      throw error;
+    }
+  }
+
+  const handleIndividualApiCall = async (url: string, index: number) => {
+    try {
+      await HandleGlobalApiRequest(url, index);
+      console.log(apiResponseData);
+    } catch (error) {
+      console.log('An error occurred');
+      setErrorDisplay(true)
+      setCardStates((prevStates) => [
+        ...prevStates.slice(0, index),
+        {
+          ctaText: 'FireApiCall',
+          isLoading: false, // Settting loading to false when there is an error
+        },
+        ...prevStates.slice(index + 1),
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    errorDisplay === true? setShowErrorDiv(true) : setShowErrorDiv(false)
+  });
+
+  const cardData: any = [
+    { title: 'Handling 200 OK', ctaText: 'Fire API Call', url: 'https://httpstat.us/200' },
+    { title: 'Handling 200 OK with 10sec delay', ctaText: 'Fire API Call', url: 'https://httpstat.us/200?sleep=10000' },
+    { title: 'Handling 400 Bad Request', ctaText: 'Fire API Call', url: 'https://httpstat.us/400' },
+    { title: 'Handling 404 Not Found ', ctaText: 'Fire API Call', url: 'https://httpstat.us/404' },
+    { title: 'Handling 500 Internal Server Error', ctaText: 'Fire API Call', url: 'https://httpstat.us/500' },
+    { title: 'Handling 504 Gateway Timeout', ctaText: 'Fire API Call', url: 'https://httpstat.us/504' },
+  ];
+
+  const errorText = 'An error occurred';
+
+  const ErrorDiv = () => {
+    return (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center  justify-center">
+        <div className="z-50 w-[300px] h-[300px] bg-white rounded-2xl p-4 items-center ring-2 ring-blue-400 justify-center p-10">
+          <div className="text-red-600 text-lg text-center">An error occurred, This is either there was a time out or the server of the api/service was unavailable</div>
+
+          <div className=" w-full flex flex-col justify-center self-center pt-6">
+            <button className="flex flex-col text-center justify-center ring-4 ring-pink-300 items-center bg-blue-600 rounded-2xl p-4"
+            onClick={()=>{setErrorDisplay(false); setShowErrorDiv(false)}}
+            >Go back</button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
+  };
+  return (
+    <div className="flex flex-row w-full h-full flex-wrap justify-center bg-white items-center lg:px-48 lg:py-16">
+      {showErrorDiv===true? <ErrorDiv/> : <></>}
+      {cardData.map((cardItem: any, index: number) => (
+        <div key={`card-items-${index}`} className="flex flex-col md:flex-row flex-wrap gap-y-4 gap-x-4 p-4">
+          <MainCard
+            title={cardItem.title}
+            errorText={errorText}
+            CtaText={cardStates[index].isLoading ? <LoadingSpinner/> : cardStates[index].ctaText}
+            onClickFunction={() => handleIndividualApiCall(cardItem.url, index)}
+            responseDescription={cardStates[index].responseDescription}
+            responseStatus={cardStates[index].responseStatus}
+            textColor={cardStates[index].textColor}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
---
+++ api-handling/src/components/MainCard.tsx
@@ -0,0 +1,52 @@
+import React, {useState,useEffect} from "react";
+
+
+const MainCard = ({
+title,
+errorText,
+CtaText,
+onClickFunction,
+responseStatus,
+responseDescription,
+textColor,
+
+}:
+{
+title:string,
+errorText:string,
+CtaText:string,
+onClickFunction:(val:any)=>(void),
+responseStatus:string,
+responseDescription:string,
+textColor:string;
+
+}) => {
+
+    return(
+
+        <div className="min-w-[218px] min-h-[218px] max-w-[218px] max-h-[218px]  flex flex-col w-full rounded-2xl bg-green-200 ring-2 ring-blue-900 py-6 px-6 gap-4 justify-center items-center"
+        >
+
+            <div className="flex flex-col text-[18px] line-[14px] text-blue-400 text-center"> {title}</div>
+            
+            <div className="flex flex-col text-blue-600 text-[14px] text-center"> 
+            <div className={` flex flex-col justify-center text-center`}>Response status:<span className={`${textColor}`}>{responseStatus}</span> </div> 
+            <div className={` flex flex-col justify-center text-center`}>Response description:<span className={`${textColor}`}> {responseDescription}</span></div>
+            </div>
+
+            <div>
+
+                <button className="flex bg-blue-500 text-[12px] text-white p-2 rounded-2xl ring-4 ring-teal-200"
+                    onClick={(val)=>{onClickFunction(val)}}
+                >
+                    {CtaText}
+
+                </button>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MainCard;
